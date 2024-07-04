9addefd2b18394fd47cecb3976088c656b85a256
jamleston <jamlessston@gmail.com>
2024-07-04 18:12:46 +0200

    fix first

--- first.py
+++ first.py
@@ -1,13 +1,22 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+
+# user_input = input('please give num:')
 
 def get_days_from_today(date_string):
-    new_date = datetime.strptime(date_string,"%Y-%m-%d")
-    new_date = new_date.date()
-    today_date = datetime.today().date()
-    # print(new_date)
-    # print(today_date)
-    timedelta = new_date - today_date
-    return timedelta.days
+        new_date = datetime.strptime(date_string,"%Y-%m-%d")
+        new_date = new_date.date()
+        today_date = datetime.today().date()
+        # print(new_date)
+        # print(today_date)
+        timedelta = new_date - today_date
+        return timedelta.days
+    # except ValueError:
+    #         print("Oops!  That was no valid number.  Try again with this format 'year-month-day'")
+    #         user_date = input('--->')
+    #         result = get_days_from_today(user_date)
+    #         return result
 
+# user_input = input('please give num:')
 result = get_days_from_today('2020-10-09')
 print(result)
+# '2020-11-09'
---
+++ second.py
@@ -0,0 +1,3 @@
+def get_numbers_ticket(min, max, quantity):
+    final_list = []
+    return final_list
