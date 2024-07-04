0cacd0b8d11b51a6bd5296ddbfb02406be718f61
jamleston <jamlessston@gmail.com>
2024-07-04 18:29:18 +0200

    start second task

--- second.py
+++ second.py
@@ -1,3 +1,16 @@
 def get_numbers_ticket(min, max, quantity):
     final_list = []
+    if min>=1 and max>=1 and max<=1000 and quantity<=max-min+1:
+        print('yay')
+    else:
+        print(final_list)
+        return final_list
+    print(final_list)
     return final_list
+
+input_min = int(input('min: '))
+input_max = int(input('max: '))
+input_quantity = int(input('quantity: '))
+
+
+get_numbers_ticket(input_min, input_max, input_quantity)
